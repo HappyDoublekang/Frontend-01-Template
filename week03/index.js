01f06ba5ce6c1bd0df56d249189234f365cf623d
SaberArthur <467455219@qq.com>
2020-04-29 22:28:27 +0800

    update

---
+++ week03/index.js
@@ -0,0 +1,42 @@
+// NumberTOSTRING
+function convertNumberToString(number, x = 10) {
+    let integer = Math.floor(number);
+    let decimal = number - integer;
+    let string = !integer ? '0' : '';
+    while (integer > 0) {
+      string = `${integer % x}${string}`;
+      integer = Math.floor(integer / x);
+    }
+  
+    if (decimal) {
+      string += '.';
+      while (decimal && !/\.\d{20}$/.test(string)) {
+        decimal *= x;
+        string += `${Math.floor(decimal)}`;
+        decimal -= Math.floor(decimal);
+      }
+    }
+    return string;
+}
+
+// StringToNumber
+function convertStringToNumber(chars, x = 10) {
+    if (/[^\d\.]/g.test(chars)) {
+      throw Error(`${chars} 并不是一个合法的数字`);
+    }
+    
+    const zeroCodePoint = '0'.codePointAt(0);
+    let integer = 0;
+    let i = 0;
+    for (; i < chars.length && chars[i] !== '.'; i++) {
+      integer *= x;
+      integer += chars[i].codePointAt(0) - zeroCodePoint;
+    }
+  
+    let decimal = 0;
+    for (let j = chars.length - 1; i < j; j--) {
+      decimal += chars[j].codePointAt(0) - zeroCodePoint;
+      decimal /= x;
+    }
+    return integer + decimal;
+}
